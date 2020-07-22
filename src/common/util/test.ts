033d5795b7aae1af5e992c1093bf0d3326c70ba6
jupl <juan.placencia.512@gmail.com>
2020-07-21 19:56:50 -0500

    Isolate test files

--- src/common/util/test.ts
+++ src/common/util/test.ts
@@ -1,6 +1,5 @@
-import {getRoot} from '.'
-
-test('getRoot', () => {
+test('getRoot', async () => {
+  const {getRoot} = await import('.')
   expect(document.querySelector('#root')).toBeFalsy()
   getRoot()
   expect(document.querySelector('#root')).toBeTruthy()
@@ -8,19 +7,22 @@ test('getRoot', () => {
   expect(document.querySelector('#root')).toBeTruthy()
 })
 
-test('getRoot - custom id', () => {
+test('getRoot - custom id', async () => {
+  const {getRoot} = await import('.')
   expect(document.querySelector('#alt')).toBeFalsy()
   getRoot({id: 'alt'})
   expect(document.querySelector('#alt')).toBeTruthy()
 })
 
-test('getRoot - custom tag', () => {
+test('getRoot - custom tag', async () => {
+  const {getRoot} = await import('.')
   expect(document.querySelector('#span')).toBeFalsy()
   getRoot({id: 'span', tag: 'span'})
   expect(document.querySelector('#span')).toBeTruthy()
 })
 
-test('getRoot - custom parent', () => {
+test('getRoot - custom parent', async () => {
+  const {getRoot} = await import('.')
   const parent = document.createElement('div')
   expect(parent.querySelector('#parent')).toBeFalsy()
   getRoot({id: 'parent', parent})
